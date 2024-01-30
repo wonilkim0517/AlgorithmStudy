29012512b5c032c926b987f32abb8305bd29224c
gitkim11 <150337530+gitkim11@users.noreply.github.com>
2024-01-30 16:52:09 +0900

    [Silver III] Title: N과 M (3), Time: 1600 ms, Memory: 115460 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,10 @@
+import sys
+from itertools import product
+
+N, M = map(int, input().split())
+
+elements = [int(i) + 1 for i in range(N)]
+productList = list(product(elements, repeat=M))
+
+for productPrint in productList:
+    print(' '.join(map(str, productPrint)))
