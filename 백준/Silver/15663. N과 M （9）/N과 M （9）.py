fa8478eb888e86728979c53775f59ad614163f55
gitkim11 <150337530+gitkim11@users.noreply.github.com>
2024-02-01 16:54:25 +0900

    [Silver II] Title: N과 M (9), Time: 4360 ms, Memory: 35732 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,14 @@
+import sys
+from itertools import permutations
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+elements = list(map(int, input().split()))
+
+permutationsList = list(set(permutations(elements, M)))
+permutationsList.sort()
+
+for permutationPrint in permutationsList:
+    if permutationPrint in permutationsList:
+        print(' '.join(map(str, permutationPrint)))
