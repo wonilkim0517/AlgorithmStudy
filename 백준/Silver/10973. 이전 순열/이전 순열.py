86263b9ca4466bce1f75a6acb9bf8e30a5501f2b
gitkim11 <150337530+gitkim11@users.noreply.github.com>
2024-02-07 16:41:56 +0900

    [Silver III] Title: 이전 순열, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(N - 1, 0, -1):
+    if arr[i - 1] > arr[i]:
+        for j in range(N - 1, 0, -1):
+            if arr[i - 1] > arr[j]:
+                arr[i - 1], arr[j] = arr[j], arr[i - 1]
+                arr = arr[:i] + sorted(arr[i:], reverse=True)
+                print(*arr)
+                exit()
+print(-1)
