35e65b7d5e0c00d87908d2517ee779b184107c10
gitkim11 <150337530+gitkim11@users.noreply.github.com>
2024-02-15 15:43:56 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver IV/9012. 괄호/괄호.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+def check(a):
+    stack = []
+    for k in a:
+        if k == '(':
+            stack.append(k)
+        elif k == ')':
+            if len(stack) == 0:
+                print("NO")
+                return
+            else:
+                stack.pop()
+
+    if len(stack) == 0:
+        print("YES")
+        return
+    else:
+        print("NO")
+        return
+
+for i in range(T):
+    b = list(input())
+    check(b)
