4f7930f5de775e141671bb1362acc4b6479409c6
gitkim11 <150337530+gitkim11@users.noreply.github.com>
2024-02-28 17:29:39 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0] * (N + 1)
+T = []
+P = []
+
+for _ in range(N):
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+
+for i in range(N - 1, -1, -1):
+    if i + T[i] > N:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], P[i] + dp[i + T[i]])
+
+print(dp[0])
