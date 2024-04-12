34204c5c92e63015d7809098c388cdae43111ed9
gitkim11 <150337530+gitkim11@users.noreply.github.com>
2024-04-12 16:55:59 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 772 ms, Memory: 69324 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -3,17 +3,6 @@ from collections import defaultdict
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-
-graph = defaultdict(list)
-visited = [False] * (N + 1)
-count = 0
-
-for _ in range(M):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-
 
 def dfs(start):
     stack = [start]
@@ -22,11 +11,29 @@ def dfs(start):
         current = stack.pop()
         if not visited[current]:
             visited[current] = True
-            stack.extend(neighbour for neighbour in graph[current] if not visited[neighbour])
+            for neighbor in graph[current]:
+                if not visited[neighbor]:
+                    stack.append(neighbor)
 
-for u in range(1, N + 1):
-    if not visited[u]:
-        dfs(u)
-        count += 1
 
-print(count)
+def count_connected_components():
+    count = 0
+    for u in range(1, N + 1):
+        if not visited[u]:
+            dfs(u)
+            count += 1
+    return count
+
+
+N, M = map(int, input().split())
+
+graph = defaultdict(list)
+visited = [False] * (N + 1)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+result = count_connected_components()
+print(result)
