a0692859389bca8cbac9317373a00c4015430669
Kim wonil <150337530+wonilkim0517@users.noreply.github.com>
2024-08-06 00:43:40 +0900

    [Silver II] Title: 결혼식, Time: 56 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.py
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -3,33 +3,31 @@ from collections import deque
 
 input = sys.stdin.readline
 
-def bfs(start):
-    q = deque()
-    q.append(start)
-    visited[start] = True
-
-    while q:
-        current = q.popleft()
-        for neighbor in graph[current]:
-            if not visited[neighbor]:
-                q.append(neighbor)
-                visited[neighbor] = visited[current] + 1
-
-
 n = int(input())
 m = int(input())
+
 graph = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
+cnt = 0
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-bfs(1)
-result = 0
-for i in range(2, n + 1):
-    if 1 <= visited[i] <= 3:
-        result += 1
+def bfs(start):
+    global cnt
+    q = deque([(start, 0)])
+    visited[start] = True
+    while q:
+        x, depth = q.popleft()
+        if depth>=2:
+            continue
+        for i in graph[x]:
+            if not visited[i]:
+                q.append((i, depth + 1))
+                visited[i] = True
+                cnt += 1
+    return cnt
 
-print(result)
+print(bfs(1))
