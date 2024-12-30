0c2713eb317299982820da8279407306c8c3db7b
Kim wonil <150337530+wonilkim0517@users.noreply.github.com>
2024-12-30 16:11:59 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,16 @@
 def solution(array, commands):
     answer = []
 
-    # new_array = array[commands[0]:commands[1]]
     for command in commands:
-        new_array = array[command[0] - 1 : command[1]]
+        i = command[0]
+        j = command[1]
+        k = command[2]
+        # i번째부터 j번째까지 짜른 배열 추출
+        new_array = array[i - 1 : j]
+        # 배열 정렬
         new_array.sort()
-        answer.append(new_array[command[2] - 1])
+        # 배열의 k번째 값을 answer에 저장
+        answer.append(new_array[k - 1])
+        # 배열 초기화
         new_array = array
     return answer
