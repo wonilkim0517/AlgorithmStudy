a9b035699cb409ce5a0bdd601371310b0da37e22
Kim wonil <150337530+wonilkim0517@users.noreply.github.com>
2025-02-15 23:23:54 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,36 +1,26 @@
+from collections import defaultdict
+
 def solution(genres, plays):
     answer = []
-    total_plays = {}
-    song_genres = {}
+    genres_dict = defaultdict(list)
+    plays_dict = defaultdict(int)
     
-    # for 문 i에 해당하는 데이터 처리
     for i in range(len(genres)):
         genre = genres[i]
         play = plays[i]
         
-        # 총 재생 수 순으로 정렬
-        if genre not in total_plays:
-            total_plays[genre] = play
-        else:
-            total_plays[genre] += play
-        
-        # 장르 별로 노래 모음
-        if genre not in song_genres:
-            song_genres[genre] = [(play, i)]
-        else:
-            song_genres[genre].append((play, i))
-            
-    # 조회수 순으로 정렬
-    total_plays = sorted(total_plays.items(), key=lambda x : -x[1])
+        genres_dict[genre].append((i, play))
+        plays_dict[genre] += play
     
-    for genre, _ in total_plays:
-        songs = sorted(song_genres[genre], key=lambda x : (-x[0], x[1]))
-        print(songs)
+    sorted_plays_dict = sorted(plays_dict.items(), key=lambda x : -x[1])
+    
+    for genre, _ in sorted_plays_dict:
+        sorted_genres_dict = sorted(genres_dict[genre], key=lambda x:(-x[1], x[0]))
+        
         count = 0
-        for song in songs:
+        for k in sorted_genres_dict:
             if count >= 2:
                 break
-            answer.append(song[1])
+            answer.append(k[0])
             count += 1
-            
     return answer
