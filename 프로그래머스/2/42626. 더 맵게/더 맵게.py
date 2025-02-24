967f79695dd9d85d9b1946d1e1ecd11c4ede1653
Kim wonil <150337530+wonilkim0517@users.noreply.github.com>
2025-02-24 16:31:01 +0900

    [level 2] Title: 더 맵게, Time: 1618.32 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -3,20 +3,16 @@ import heapq
 def solution(scoville, K):
     answer = 0
     
-    # 리스트를 최소 힙으로 변환
     heapq.heapify(scoville)
     
-    # 첫 번째 값이 K 다 클 때 까지
     while scoville[0] < K:
         if len(scoville) < 2:
             return -1
         
-        # 가장 작은 두 값 추출
-        fisrt = heapq.heappop(scoville)
+        first = heapq.heappop(scoville)
         second = heapq.heappop(scoville)
-
-        # 섞은 음식의 스코빌 지수
-        mix_scoville = fisrt + (second * 2)
+        
+        mix_scoville = first + (second * 2)
         heapq.heappush(scoville, mix_scoville)
         answer += 1
     return answer
